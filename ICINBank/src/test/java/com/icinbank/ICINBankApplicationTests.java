6a2528fada2b6d2eef126ba45dda7f662aef01a4
unknown <prasadnithi15@gmail.com>
2020-08-15 19:27:13 +0530

    user portal

---
+++ ICINBank/src/test/java/com/icinbank/ICINBankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.icinbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ICINBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
